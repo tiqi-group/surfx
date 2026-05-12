a222d07b45fec0244bdd51cef040970356e412c6
Alfredo Ricci Vasquez <alfredoricciv@gmail.com>
2026-05-12 14:26:39 +0200

    Tests

--- surfx/tests/test_electrode.py
+++ surfx/tests/test_electrode.py
@@ -1,270 +1,113 @@
-# -*- coding: utf8 -*-
-#
-#   electrode.py: numeric tools for Paul traps
-#
-#   Copyright (C) 2011 Robert Jordens <jordens@phys.ethz.ch>
-#
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-from __future__ import (absolute_import, print_function,
-        unicode_literals, division)
-
-import os
 import unittest
 
 import numpy as np
 from numpy import testing as nptest
 
-from surfx import utils, electrode, system
+from surfx.potential import electrode_potential
+from surfx.utils import expand_tensor
 
+# TODO: Need to add this once the cover is added
+# class CoverCase(unittest.TestCase):
+#     def setUp(self):
+#         self.c = electrode.CoverElectrode(height=20)
+#         self.x = np.array([[1, 2, 3.]])
 
-class CoverCase(unittest.TestCase):
-    def setUp(self):
-        self.c = electrode.CoverElectrode(height=20)
-        self.x = np.array([[1, 2, 3.]])
+#     def test_pot(self):
+#         nptest.assert_almost_equal(self.c.potential(
+#             self.x, 0), [[3/20.]])
 
-    def test_pot(self):
-        nptest.assert_almost_equal(self.c.potential(
-            self.x, 0), [[3/20.]])
+#     def test_grad(self):
+#         nptest.assert_almost_equal(self.c.potential(
+#             self.x, 1), [[0, 0, 1/20.]])
 
-    def test_grad(self):
-        nptest.assert_almost_equal(self.c.potential(
-            self.x, 1), [[0, 0, 1/20.]])
+#     def test_curve(self):
+#         nptest.assert_almost_equal(self.c.potential(
+#             self.x, 2).sum(), 0.)
 
-    def test_curve(self):
-        nptest.assert_almost_equal(self.c.potential(
-            self.x, 2).sum(), 0.)
-
-    def test_orientation(self):
-        nptest.assert_almost_equal(self.c.orientations(), [])
-
-
-class LargeElectrodeCase(unittest.TestCase):
-    def setUp(self):
-        r = 1e9
-        self.e = electrode.PolygonPixelElectrode(paths=[[
-            [r, r], [-r, r], [-r, -r], [r, -r]]])
-        self.x = np.array([[1, 2, 3.]])
-    
-    def test_pot(self):
-        nptest.assert_almost_equal(self.e.potential(
-            self.x, 0)[0], 1)
-
-    def test_pot_cover(self):
-        #need large numbers due to large electrode
-        self.e.cover_nmax = int(1e5)
-        self.e.cover_height = 1e6
-        x = np.array([[0, 0, self.e.cover_height]])
-        nptest.assert_allclose(self.e.potential(x, 0), 0, atol=1e-4)
-
-    def test_z_symmetry(self):
-        x = self.x*[[1, 1, 1,], [1, 1, -1]]
-        for i, deriv in enumerate(utils._derivative_names):
-            s = -(-1)**np.array([_.count("z") for _ in deriv])
-            p = self.e.potential(x, i)
-            nptest.assert_allclose(s*p[0], p[1])
-
-    def test_grad(self):
-        nptest.assert_allclose(self.e.potential(
-            self.x, 1), 0, atol=1e-9)
-
-    def test_curve(self):
-        nptest.assert_allclose(self.e.potential(
-            self.x, 2)[0], 0., atol=1e-9)
-
-    def test_orientation(self):
-        nptest.assert_almost_equal(self.e.orientations(), [1.])
-
-
-class PixelElectrodeCase(unittest.TestCase):
-    def setUp(self):
-        self.r = r = 4e-5
-        self.p = electrode.PolygonPixelElectrode(paths=[[
-            [1+r, 2+r], [1-r, 2+r], [1-r, 2-r], [1+r, 2-r]]])
-        a = (2*r)**2
-        self.e = electrode.PointPixelElectrode(areas=[a], points=[[1, 2]])
-        self.x = np.array([[3, 6, 5.]])
-
-    def test_convert(self):
-        c = self.p.to_points()
-        nptest.assert_almost_equal(c.areas, self.e.areas, 13)
-        nptest.assert_almost_equal(c.points, self.e.points)
-
-    def test_pots(self):
-        for di in range(6):
-            a = self.p.potential(self.x, di)
-            b = self.e.potential(self.x, di)
-            nptest.assert_allclose(a, b, rtol=1e-4)
-
-    def test_z_symmetry(self):
-        x = self.x*[[1, 1, 1,], [1, 1, -1]]
-        for i, deriv in enumerate(utils._derivative_names):
-            s = -(-1)**np.array([_.count("z") for _ in deriv])
-            p = self.e.potential(x, i)
-            nptest.assert_allclose(s*p[0], p[1])
-            q = self.p.potential(x, i)
-            nptest.assert_allclose(s*q[0], q[1])
-
-    def test_orientation(self):
-        nptest.assert_almost_equal(self.e.orientations(), [1.])
-        nptest.assert_almost_equal(self.p.orientations(), [1.])
-
-    def test_derivs(self):
-        ns = range(1, 6)
-        for ee, d in (self.e, 1e-6), (self.p, 1e-6):
-            xd = self.x + [[0, 0, 0], [d, 0, 0], [0, d, 0], [0, 0, d]]
-            for n in ns:
-                p = utils.expand_tensor(ee.potential(self.x, n))[0]
-                pd = utils.expand_tensor(ee.potential(xd, n-1))
-                pd = (pd[1:] - pd[0])/d
-                nptest.assert_allclose(p, pd, rtol=d, atol=1e-14,
-                      err_msg="ee=%s, n=%i" % (ee, n))
+#     def test_orientation(self):
+#         nptest.assert_almost_equal(self.c.orientations(), [])
 
 
 class PolygonTestCase(unittest.TestCase):
     def setUp(self):
-        p = np.array([[1, 0], [2, 3], [2, 7], [3, 8],
-            [-2, 8], [-5, 2]])
-        self.e = electrode.PolygonPixelElectrode(paths=[p])
-        self.x = np.array([[1, 2, 3.]])
-
-    def test_orientation(self):
-        nptest.assert_almost_equal(self.e.orientations(), [1.])
+        self.polygon = np.array([[1, 0], [2, 3], [2, 7], [3, 8], [-2, 8], [-5, 2]])
+        self.x = 1
+        self.y = 2
+        self.z = 3
+        self.positions = (self.x, self.y, self.z)
+        self.cover_height = 50.0
+        self.cover_nmax = 0
+        self.voltage = 1.0
 
     def test_pot_cover(self):
-        self.e.cover_nmax = 10
-        self.e.cover_height = 50.
-        x = np.array([[0, 0, self.e.cover_height]])
-        # phi = 0
-        nptest.assert_allclose(self.e.potential(x, 0), 0, atol=1e-5)
-        # E_perp == 0
-        nptest.assert_allclose(self.e.potential(x, 1)[:, :2], 0,
-                atol=1e-5)
+        cover_positions = (0, 0, self.cover_height)
+
+        # potential should be zero at the cover height
+        nptest.assert_allclose(
+            electrode_potential(
+                self.polygon,
+                0.0,
+                *cover_positions,
+                0,
+                self.cover_height,
+                cover_nmax=10,
+            ),
+            0,
+            atol=1e-5,
+        )
+
+        # field should be zero in direction at the cover height
+        nptest.assert_allclose(
+            electrode_potential(
+                self.polygon,
+                0.0,
+                *cover_positions,
+                1,
+                self.cover_height,
+                cover_nmax=10,
+            ),
+            0,
+            atol=1e-5,
+        )
 
     def test_known_pot(self):
         nptest.assert_almost_equal(
-                self.e.potential(self.x, 0)[0], .24907)
+            electrode_potential(
+                self.polygon,
+                self.voltage,
+                *self.positions,
+                0,
+                self.cover_height,
+                cover_nmax=self.cover_nmax,
+            ),
+            0.24907,
+        )
 
     def test_known_grad(self):
         nptest.assert_almost_equal(
-                self.e.potential(self.x, 1)[0],
-                [-0.0485227, 0.0404789, -0.076643])
+            electrode_potential(
+                self.polygon,
+                self.voltage,
+                *self.positions,
+                1,
+                self.cover_height,
+                cover_nmax=self.cover_nmax,
+            ),
+            expand_tensor(np.array([-0.0485227, 0.0404789, -0.076643])),
+        )
 
     def test_known_curve(self):
         nptest.assert_almost_equal(
-                self.e.potential(self.x, 2)[0],
-                [-0.0196946, -0.00747322, 0.0287624, -0.014943, -0.0182706])
-
-    def test_derivs(self):
-        ns = range(1, 6)
-        d = 1e-7
-        xd = self.x + [[0, 0, 0], [d, 0, 0], [0, d, 0], [0, 0, d]]
-        for n in ns:
-            p = utils.expand_tensor(self.e.potential(self.x, n))[0]
-            pd = utils.expand_tensor(self.e.potential(xd, n-1))
-            pd = (pd[1:] - pd[0])/d
-            for k, (i, j) in enumerate(zip(p.ravel(), pd.ravel())):
-                nptest.assert_allclose(i, j, rtol=d, atol=d/100,
-                      err_msg="n=%i, k=%i" % (n,k))
-                
-    def test_pseudopotential_derivs(self):
-        ns = range(1, 5)
-        d = 1e-7
-        xd = self.x + [[0, 0, 0], [d, 0, 0], [0, d, 0], [0, 0, d]]
-        s = system.System([self.e])
-        self.e.rf = 1.
-        for n in ns:
-            p = s.potential(self.x, n)[0]
-            pd = s.potential(xd, n-1)
-            pd = (pd[1:] - pd[0])/d
-            for k, (i, j) in enumerate(zip(p.ravel(), pd.ravel())):
-                nptest.assert_allclose(i, j, rtol=d, atol=d/100,
-                      err_msg="n=%i, k=%i" % (n,k))
-
-    def test_spherical_harmonics(self):
-        ns = range(6)
-        v = [self.e.potential(self.x, i).T for i in ns]
-        for i, vi in enumerate(v):
-            s = utils.cartesian_to_spherical_harmonics(vi)
-            self.assertEqual(s.shape, vi.shape)
-
-    def test_z_symmetry(self):
-        x = self.x*[[1, 1, 1,], [1, 1, -1]]
-        for i, deriv in enumerate(utils._derivative_names):
-            s = -(-1)**np.array([_.count("z") for _ in deriv])
-            p = self.e.potential(x, i)
-            nptest.assert_allclose(s*p[0], p[1])
-
-
-class GridElectrodeCase(unittest.TestCase):
-    def setUp(self):
-        p = np.array([[1, 0], [2, 3], [2, 7], [3, 8],
-            [-2, 8], [-5, 2]])
-        self.p = electrode.PolygonPixelElectrode(paths=[p])
-        spacing = .11, .13, .11
-        origin = -1.5, -1.7, .9
-        shape = 31, 32, 33
-        x = np.mgrid[[slice(o, o+(s-.5)*d, d) for o, s, d in zip(origin,
-            shape, spacing)]]
-        xt = x.reshape(3, -1).T
-        assert x.shape == (3,) + shape, x.shape
-        pot0 = self.p.potential(x.reshape(3, -1).T,
-                0).reshape(x[0].shape+(1,))
-        pot1 = self.p.potential(x.reshape(3, -1).T,
-                1).reshape(x[0].shape+(3,))
-        self.e = electrode.GridElectrode(data=[pot0, pot1],
-            origin=origin, spacing=spacing)
-
-    def test_gen(self):
-        for i in 0, 5:
-            self.e.generate(maxderiv=i)
-
-    def test_pot(self):
-        self.e.generate(5)
-        x = np.array([[1.4567, 1.67858, 1.49533]])
-        for d, r in (0, 1e-3), (1, 2e-3), (2, 5e-3), (3, .1), (4, .5):
-            pe = self.e.potential(x, d)
-            pp = self.p.potential(x, d)
-            nptest.assert_allclose(pe, pp, rtol=r, atol=1e-4)
-
-
-# @unittest.skipIf(electrode.tvtk is None, "no tvtk")
-class GridElectrodeVtkCase(unittest.TestCase):
-    fil = "~/work/nist/qc-tools/trunk/bin/threefold_2_sim_dense_rf.vtk"
-    fil = os.path.expanduser(fil)
-    @unittest.skipUnless(os.path.exists(fil), "no dataset")
-    def test_load_vtk(self):
-        e = electrode.GridElectrode.from_vtk(os.path.expanduser(self.fil))
-
-
-class MeshElectrodeCase(unittest.TestCase):
-    def setUp(self):
-        p = np.array([[0, 0], [1, 0], [1, 2], [2, 2.]])
-        a = electrode.PolygonPixelElectrode(paths=[p, p+[[3, 3.]]],
-                dc=1.)
-        b = electrode.PolygonPixelElectrode(paths=[p-[[3, 3]]],
-                dc=2.)
-        self.s = system.System([a, b])
-        self.m = electrode.MeshPixelElectrode.from_polygon_system(
-            self.s)
-        self.x = np.array([[1,2,3.]])
-
-    def test_create(self):
-        for di in range(6):
-            a = self.s.electrical_potential(self.x, "dc", di)
-            b = self.m.potential(self.x, di)
-            nptest.assert_allclose(a, b)
+            electrode_potential(
+                self.polygon,
+                self.voltage,
+                *self.positions,
+                2,
+                self.cover_height,
+                cover_nmax=self.cover_nmax,
+            ),
+            expand_tensor(np.array([-0.0196946, -0.00747322, 0.0287624, -0.014943, -0.0182706])),
+        )
 
 
 if __name__ == "__main__":
--- surfx/tests/test_utils.py
+++ surfx/tests/test_utils.py
@@ -1,195 +1,39 @@
-# -*- coding: utf8 -*-
-#
-#   electrode.py: numeric tools for Paul traps
-#
-#   Copyright (C) 2011 Robert Jordens <jordens@phys.ethz.ch>
-#
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-from __future__ import (absolute_import, print_function,
-        unicode_literals, division)
-
 import unittest
 
 import numpy as np
 from numpy import testing as nptest
-try:
-    from scipy.special import sph_harm_y as _sph_harm_y
-    def sph_harm(m, n, theta, phi):
-        # old: theta=azimuthal [0,2pi], phi=polar [0,pi]
-        # new: theta=polar [0,pi], phi=azimuthal [0,2pi]
-        return _sph_harm_y(n, m, phi, theta)
-except ImportError:
-    from scipy.special import sph_harm
 
-from surfx import transformations, utils, electrode
+from surfx.expressions import polygon_potential  # TODO: Deleteme
+from surfx.utils import expand_tensor
 
 
 class BasicFunctionsCase(unittest.TestCase):
-    def test_dummy_pool(self):
-        f = lambda x, y=1, *a, **k: (x, y, a, k)
-        r = utils.DummyPool().apply_async(f, (2, 3, 4), {"a": 5})
-        self.assertEqual(r.get(), (2, 3, (4,), {"a": 5}))
-
-    def test_apply_method(self):
-        class C:
-            def m(self, a):
-                return a
-        self.assertEqual(utils.apply_method(C(), "m", 1), 1)
-
-    def test_norm(self):
-        self.assertEqual(utils.norm([1,2,3.]), 14**.5)
-        self.assertEqual(utils.norm([[1,2,3.]], 1), 14**.5)
-
     def test_expand_tensor(self):
-        a = np.array([1, 2, 3.])[None, :]
-        nptest.assert_equal(utils.expand_tensor(a), a)
+        a = np.array([1, 2, 3.0])[None, :]
+        nptest.assert_equal(expand_tensor(a), a)
         b = np.array([1, 2, 3, 4, 5])[None, :]
-        b1 = np.array([1, 2, 3, 2, 4, 5, 3, 5, -5] # triu
-                ).reshape((1, 3, 3))
-        nptest.assert_equal(utils.expand_tensor(b), b1)
+        b1 = np.array(
+            [1, 2, 3, 2, 4, 5, 3, 5, -5]  # triu
+        ).reshape((1, 3, 3))
+        nptest.assert_equal(expand_tensor(b), b1)
         c = np.random.random(5)
         ti, tj = np.triu_indices(3)
-        ce = utils.expand_tensor(c[None, :])[0, ti, tj]
+        ce = expand_tensor(c[None, :])[0, ti, tj]
         nptest.assert_equal(ce[:5], c)
-        nptest.assert_equal(ce[5], -c[0]-c[3])
-    
-    def test_expand_select_tensor(self):
-        for n in 3, 5, 7, 9, 11:
-            d = np.random.random(n)[None, :]
-            de = utils.expand_tensor(d)
-            ds = utils.select_tensor(de)
-            nptest.assert_equal(d, ds)
-
-    def test_expand_tensor_trace(self):
-        d = np.random.random(5)[None, :]
-        de = utils.expand_tensor(d)
-        nptest.assert_almost_equal(de[0].trace(), 0)
-        d = np.random.random(7)[None, :]
-        de = utils.expand_tensor(d)
-        nptest.assert_almost_equal(de[0].trace(), np.zeros((3)))
-        d = np.random.random(9)[None, :]
-        de = utils.expand_tensor(d)
-        nptest.assert_almost_equal(de[0].trace(), np.zeros((3,3)))
-        d = np.random.random(11)[None, :]
-        de = utils.expand_tensor(d)
-        nptest.assert_almost_equal(de[0].trace(), np.zeros((3,3,3)))
-
-    def test_rotate_tensor_identity(self):
-        dr = np.identity(3)
-        d = np.arange(3).reshape((1,3,))
-        nptest.assert_almost_equal(d, utils.rotate_tensor(d, dr, 1))
-        d = np.arange(3**2).reshape((1,3,3))
-        nptest.assert_almost_equal(d, utils.rotate_tensor(d, dr, 2))
-        d = np.arange(3**3).reshape(1,3,3,3)
-        nptest.assert_almost_equal(d, utils.rotate_tensor(d, dr, 3))
-        d = np.arange(3**4).reshape(1,3,3,3,3)
-        nptest.assert_almost_equal(d, utils.rotate_tensor(d, dr, 4))
-        d = np.arange(3**2*5).reshape(5,3,3)
-        nptest.assert_almost_equal(d, utils.rotate_tensor(d, dr, 2))
-        d = np.arange(3**4*5).reshape(5,3,3,3,3)
-        nptest.assert_almost_equal(d, utils.rotate_tensor(d, dr, 4))
-    
-    def test_rotate_tensor_rot(self):
-        r = transformations.euler_matrix(*np.random.random(3))[:3, :3]
-        d = np.arange(3**3*5).reshape(5,3,3,3)
-        dr = utils.rotate_tensor(d, r, 3)
-        drr = utils.rotate_tensor(dr, r.T, 3)
-        nptest.assert_almost_equal(d, drr)
-
-    def test_rotate_tensor_simple(self):
-        r = transformations.euler_matrix(0, 0, np.pi/2, "sxyz")[:3, :3]
-        d = np.arange(3)
-        nptest.assert_almost_equal(d[[1, 0, 2]],
-                utils.rotate_tensor(d, r, 1))
-        d = np.arange(9).reshape(1,3,3)
-        nptest.assert_almost_equal([[[4, -3, 5], [-1, 0, -2], [7, -6, 8]]],
-                utils.rotate_tensor(d, r, 2))
-
-    def test_centroid_area(self):
-        p = np.array([[1, 0, 0], [2, 3, 0], [2, 7, 0], [3, 8, 0],
-            [-2, 8, 0], [-5, 2, 0.]])
-        a, c = utils.area_centroid(p)
-        nptest.assert_almost_equal(a, 40)
-        nptest.assert_almost_equal(c, [-0.7833333, 4.2083333, 0])
-
-    def test_mathieu(self):
-        a = np.array([.005])
-        q = np.array([.2**.5])
-        mu, b = utils.mathieu(1, a, q)
-        nptest.assert_almost_equal(mu.real, 0., 9)
-        mui = sorted(mu.imag[mu.imag > 0])
-        nptest.assert_almost_equal(mui[0], (a+q**2/2)**.5, 2)
-        nptest.assert_almost_equal(mui[0], [.33786], 5)
-        n = 3
-        a = np.arange(n**2).reshape(n,n)
-        q = np.arange(n**2)[::-1].reshape(n,n)*10
-        mu, b = utils.mathieu(3, a, q)
-        #nptest.assert_almost_equal(mu, [.1, .2, .3])
-        #nptest.assert_almost_equal(b, )
+        nptest.assert_equal(ce[5], -c[0] - c[3])
 
     def test_polygon_value(self):
-        p = np.array([[1., 0], [2, 3], [2, 7], [3, 8],
-            [-2, 8], [-5, 2]])
-        x = np.array([[1,2,3.]])
-        nptest.assert_almost_equal(
-                electrode.polygon_potential(x, [p], 1, 0, 0, 0, None),
-                [[.24907]])
-    
+        p = np.array([[1.0, 0], [2, 3], [2, 7], [3, 8], [-2, 8], [-5, 2]])
+        x = np.array([[1, 2, 3.0]])
+        nptest.assert_almost_equal(polygon_potential(x, [p], 1, 0, 0, 0, None), [[0.24907]])
+
     def test_polygon_value_grad(self):
-        p = np.array([[1., 0], [2, 3], [2, 7], [3, 8],
-            [-2, 8], [-5, 2]])
-        x = np.array([[1,2,3.]])
+        p = np.array([[1.0, 0], [2, 3], [2, 7], [3, 8], [-2, 8], [-5, 2]])
+        x = np.array([[1, 2, 3.0]])
         nptest.assert_almost_equal(
-                electrode.polygon_potential(x, [p], 1, 1, 0, 0, None),
-                [[-0.0485227, 0.0404789, -0.076643]])
-
-
-class SphHarmCase(unittest.TestCase):
-    def setUp(self):
-        c = np.ones(3)*0.
-        s = np.ones(3)*1.
-        self.nn = 3
-        n = np.ones(3)*(self.nn*2+1)
-        o = c-(n-1)/2*s
-        self.x = np.mgrid[[slice(oi, oi+ni*si, si) for oi, ni, si in
-                zip(o, n, s)]]
-        self.xt = self.x.reshape(3, -1).T
-        xy = np.square(self.x[:2]).sum(0)
-        self.r = np.sqrt(xy+np.square(self.x[2]))
-        self.theta = np.arctan2(np.sqrt(xy), self.x[2])
-        self.phi = np.arctan2(self.x[1], self.x[0])
-        self.e = electrode.GridElectrode(data=[], origin=o, spacing=s)
-
-    def test_analysis(self):
-        n = 6
-        for l in range(n):
-            for m in range(-l, l+1):
-                y = sph_harm(abs(m), l, self.phi, self.theta)
-                y = {-1: 2**.5*y.real, 0: y.real, 1: 2**.5*y.imag}[np.sign(m)]
-                self.e.data = [(y*self.r**l)[:, :, :, None]]
-                self.e.generate(n)
-                for k in range(n):
-                    p = self.e.potential(self.xt, k).T
-                    a = utils.cartesian_to_spherical_harmonics(p)
-                    a = a.T.reshape(self.x[0].shape + (a.shape[0],))
-                    ac = a[self.nn, self.nn, self.nn]
-                    if k == l:
-                        nptest.assert_allclose(ac, np.identity(2*l+1)[l-m],
-                                rtol=1e-12, atol=1e-12)
-                    elif not (l > k and (l-k)%2 == 0): # FIXME
-                        nptest.assert_allclose(ac, 0, atol=1e-12)
+            polygon_potential(x, [p], 1, 1, 0, 0, None),
+            [[-0.0485227, 0.0404789, -0.076643]],
+        )
 
 
 if __name__ == "__main__":
