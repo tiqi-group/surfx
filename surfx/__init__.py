1e9da003bc2b7ae4c68e70164ab7d63d8f57972e
Alfredo Ricci Vasquez <alfredoricciv@gmail.com>
2026-05-12 14:28:42 +0200

    refactor

--- surfx/__init__.py
+++ surfx/__init__.py
@@ -1,16 +1,16 @@
-from .electrode import (
-    CoverElectrode,
-    GridElectrode,
-    MeshPixelElectrode,
-    PointPixelElectrode,
-    PolygonPixelElectrode,
-)
-from .pattern_constraints import (
-    MultiPotentialObjective,
-    PatternRangeConstraint,
-    PotentialObjective,
-)
+# from ._delete.electrode import (
+#     CoverElectrode,
+#     GridElectrode,
+#     MeshPixelElectrode,
+#     PointPixelElectrode,
+#     PolygonPixelElectrode,
+# )
+# from ._delete.pattern_constraints import (
+#     MultiPotentialObjective,
+#     PatternRangeConstraint,
+#     PotentialObjective,
+# )
 from .potential import electrode_potential
-from .system import System
-from .transformations import euler_from_matrix, euler_matrix
-from .utils import shaped
+# from .system import System
+# from ._delete.transformations import euler_from_matrix, euler_matrix
+# from ._delete.utils import shaped
